0cbe639d417dd8932bdb9da694c396aff373aed1
aszeidan <38440866+aszeidan@users.noreply.github.com>
2020-04-15 17:51:57 +0300

    updated exported database

---
+++ DataBase/student.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Apr 15, 2020 at 01:51 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student_managment_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE IF NOT EXISTS `student` (
+  `StudentID` int(11) NOT NULL AUTO_INCREMENT,
+  `SFirstName` varchar(255) NOT NULL,
+  `SMiddleName` varchar(255) NOT NULL,
+  `SLastName` varchar(255) NOT NULL,
+  `SEmail` varchar(255) NOT NULL,
+  `SPhone` int(11) NOT NULL,
+  `SPassword` varchar(255) NOT NULL,
+  PRIMARY KEY (`StudentID`),
+  UNIQUE KEY `StudentID` (`StudentID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`StudentID`, `SFirstName`, `SMiddleName`, `SLastName`, `SEmail`, `SPhone`, `SPassword`) VALUES
+(1, 'Daniel', 'Marwan', 'Awde', 'dmawde@hotmail.com', 7065502, ''),
+(2, 'Ghewa', 'Salim', 'Zeidan', 'gszeidan@hotmail.com', 71655502, '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
