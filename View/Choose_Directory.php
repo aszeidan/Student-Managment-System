64204c30df7d435799adc4caa8160ff4c052bb0b
Ghida Shaaban <ghidashaaban@gmail.com>
2020-04-19 13:36:24 +0300

    Create Choose_Directory.php

---
+++ View/Choose_Directory.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+require_once('Header.php');
+?>
+
+<body>
+    <div class="container register">
+        <div class="row">
+            <div class="col-md-3 register-left">
+                <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
+                <h3>Welcome To AG University</h3>
+                <P>We look forward to welcoming you to our campus soon!​</P>
+            </div>
+            <div class="col-md-9 register-right">
+
+                <form>
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" name="teacher" aria-labelledby="home-tab">
+                        <h3 class="register-heading">Welcome Admin!</h3>
+                        <div class="row register-form mx-0 px-0">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="button" class="btnRegister" value="Registration" onClick="window.location.href='Registration.php';" tabindex="2">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="button" class="btnRegister" value="SignUp" onClick="window.location.href='SignUp.php';" tabindex="2">
+                                </div>
+                            </div>                   
+                        </div>
+                    </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+
+    <?php
+    require_once('Footer.php'); ?>
+
+</body>
+
+</html>
