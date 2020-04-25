dba6b7b277973190428626cb3c5ca0245495c860
aszeidan <38440866+aszeidan@users.noreply.github.com>
2020-04-25 17:50:16 +0300

    splitted the signup pages for teacher and the student

---
+++ View/SignUpStudent.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+
+require_once('Header.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once('../Model/DatabaseSMS.php');
+require_once('../Model/Admin.php');
+$db = new DatabaseSMS();
+$Admin = new Admin($db);
+$semester = $Admin->getSemesters();
+
+$course = $Admin->getCourses();
+
+$teacher = $Admin->getTeachers();
+
+$schedule = $Admin->getSchedules();
+
+$class = $Admin->getClasses();
+
+?>
+
+<body>
+    <div class="container register">
+        <div class="row">
+            <div class="col-md-3 register-left">
+                <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
+                <h3>Welcome To AG University</h3>
+                <P>We look forward to welcoming you to our campus soon!​</P>
+            </div>
+            <div class="col-md-9 register-right">
+                <ul class="nav nav-tabs nav-justified" id="myTab" name="myTab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Instructor</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Student</a>
+                    </li>
+                </ul>
+                <div class="row col-md-2">
+                    <ul>
+                        <li style='display: unset;position: absolute;margin: 72px;margin-left: 78px;margin-bottom: 79px;'><a href="#">Previous</a></li>
+                    </ul>
+                </div>
+                <form action="../Controller/Verify_SignUpTeacher.php" method="POST">
+                    <div class="tab-content" id="myTabContent">
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" name="teacher" aria-labelledby="home-tab">
+                            <h3 class="register-heading">Apply as a Students</h3>
+                            <div class="row register-form mx-0 px-0">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="First Name *" value="" id="SFirstName" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Middle Name *" value="" id="SMiddleName" />
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="text" class="form-control" placeholder="Last Name *" value="" id="SLastName" />
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control" placeholder="Email *" value="" id="SEmail" />
+                                    </div>
+
+                                    <div class="form-group">
+                                        <input type="text" maxlength="8" minlength="8" class="form-control" placeholder="Phone *" value="" id="SPhone" />
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="input-group mb-3">
+                                            <input type="text" class="form-control" placeholder="Password*" id="SPassword" aria-label=" Recipient's username" aria-describedby="button-addon2">
+                                            <div class="input-group-append">
+                                                <input class="btn btn-outline-primary" value="Generate" onClick="randomPassword(8,'SPassword');" tabindex="2" type="button" id="button-addon2" /> </div>
+                                        </div>
+                                        <div class="form-group" id="studentMessage">
+                                        </div>
+                                        <input type="button" onClick="createStudent()" class="btnRegister" name="signUpStudent" value="Register" />
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade show" id="profile" role="tabpanel" name="student" aria-labelledby="profile-tab">
+                            <h3 class="register-heading">Registered Students</h3>
+                            <div class="row register-form mx-0 px-0">
+                                <div class="col-md-6">
+                                    <div class=" register-form">
+
+                                        <div class="form-group">
+                                            <table border="5" class="table table-hover table-bordered width:fit content" id="Registration_table">
+
+                                                <thead class="table-primary">
+
+                                                    <th> Instructor </th>
+                                                    <th> Phone Number</th>
+                                                    <th> Email </th>
+                                                    <th>Delete </th>
+                                                    <th> Edit </th>
+                                                </thead>
+                                                </span>
+                                                <?php
+                                                for ($i = 0; $i < count($class); $i++) {
+                                                ?>
+                                                    <tr>
+                                                        <td><?php echo $class[$i]['TFirstName'] . " " . $class[$i]['TLastName']; ?> </td>
+                                                        <td><?php echo $class[$i]['TMobileNum']; ?> </td>
+                                                        <td><?php echo $class[$i]['TEmail']; ?> </td>
+                                                        <td id="delete"><a href="#" ; onclick="deleteClass(<?php echo $class[$i]['ClassId']; ?> )">Delete </a> </td>
+                                                        <td><a href="../View/EditTeacher.php?id=<?php echo $class[$i]['ClassId']; ?>">Edit </a> </td>
+                                                    </tr> <?php } ?>
+                                            </table>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </form>
+            </div>
+
+        </div>
+    </div>
+    <script>
+        function randomPassword(length, id) {
+            var chars = "abcdefghijklmnopqrstuvwxyz!@#$%^&*()-+<>ABCDEFGHIJKLMNOP1234567890";
+            var pass = "";
+            for (var x = 0; x < length; x++) {
+                var i = Math.floor(Math.random() * chars.length);
+                pass += chars.charAt(i);
+            }
+
+            document.getElementById(id).value = pass;
+        }
+
+        function callSpinner() {
+            document.getElementById("#loader").style.display = "inline";
+        }
+
+
+        function createStudent() {
+            var data = {
+                SFirstName: $("#SFirstName").val(),
+                SMiddleName: $("#SMiddleName").val(),
+                SLastName: $("#SLastName").val(),
+                SPhone: $("#SPhone").val(),
+                SPassword: $("#SPassword").val(),
+                SEmail: $("#SEmail").val()
+            };
+            $.post("../Controller/Verify_SignUpStudent.php", data, function(result) {
+                $("#studentMessage").html(result.Message);
+                setTimeout(() => {
+                    $("#studentMessage").html(" ");
+                }, 4000);
+            });
+
+        }
+    </script>
+    <?php
+    require_once('Footer.php'); ?>
+
+</body>
+
+</html>
--- View/SignUpTeacher.php
+++ View/SignUpTeacher.php
@@ -1,7 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
+
 require_once('Header.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once('../Model/DatabaseSMS.php');
+require_once('../Model/Admin.php');
+$db = new DatabaseSMS();
+$Admin = new Admin($db);
+$semester = $Admin->getSemesters();
+
+$course = $Admin->getCourses();
+
+$teacher = $Admin->getTeachers();
+
+$schedule = $Admin->getSchedules();
+
+$class = $Admin->getClasses();
+
 ?>
 
 <body>
@@ -67,36 +85,36 @@ require_once('Header.php');
                             </div>
                         </div>
                         <div class="tab-pane fade show" id="profile" role="tabpanel" name="student" aria-labelledby="profile-tab">
-                            <h3 class="register-heading">Apply as a Student</h3>
+                            <h3 class="register-heading">Registered Instructors</h3>
                             <div class="row register-form mx-0 px-0">
                                 <div class="col-md-6">
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="First Name *" value="" id="SFirstName" />
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Middle Name *" value="" id="SMiddleName" />
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" placeholder="Last Name *" value="" id="SLastName" />
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Email *" value="" id="SEmail" />
-                                    </div>
+                                    <div class=" register-form">
 
-                                    <div class="form-group">
-                                        <input type="text" maxlength="8" minlength="8" class="form-control" placeholder="Phone *" value="" id="SPhone" />
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Password*" id="SPassword" aria-label=" Recipient's username" aria-describedby="button-addon2">
-                                            <div class="input-group-append">
-                                                <input class="btn btn-outline-primary" value="Generate" onClick="randomPassword(8,'SPassword');" tabindex="2" type="button" id="button-addon2" /> </div>
+                                        <div class="form-group">
+                                            <table border="5" class="table table-hover table-bordered width:fit content" id="Registration_table">
+
+                                                <thead class="table-primary">
+
+                                                    <th> Instructor </th>
+                                                    <th> Phone Number</th>
+                                                    <th> Email </th>
+                                                    <th>Delete </th>
+                                                    <th> Edit </th>
+                                                </thead>
+                                                </span>
+                                                <?php
+                                                for ($i = 0; $i < count($class); $i++) {
+                                                ?>
+                                                    <tr>
+                                                        <td><?php echo $class[$i]['TFirstName'] . " " . $class[$i]['TLastName']; ?> </td>
+                                                        <td><?php echo $class[$i]['TMobileNum']; ?> </td>
+                                                        <td><?php echo $class[$i]['TEmail']; ?> </td>
+                                                        <td id="delete"><a href="#" ; onclick="deleteClass(<?php echo $class[$i]['ClassId']; ?> )">Delete </a> </td>
+                                                        <td><a href="../View/EditTeacher.php?id=<?php echo $class[$i]['ClassId']; ?>">Edit </a> </td>
+                                                    </tr> <?php } ?>
+                                            </table>
                                         </div>
-                                        <div class="form-group" id="studentMessage">
-                                        </div>
-                                        <input type="button" onClick="createStudent()" class="btnRegister" name="signUpStudent" value="Register" />
+
                                     </div>
                                 </div>
                             </div>
@@ -105,7 +123,6 @@ require_once('Header.php');
             </div>
 
         </div>
-    </div>
     </div>
     <script>
         function randomPassword(length, id) {
@@ -123,23 +140,6 @@ require_once('Header.php');
             document.getElementById("#loader").style.display = "inline";
         }
 
-        function createStudent() {
-            var data = {
-                SFirstName: $("#SFirstName").val(),
-                SMiddleName: $("#SMiddleName").val(),
-                SLastName: $("#SLastName").val(),
-                SPhone: $("#SPhone").val(),
-                SPassword: $("#SPassword").val(),
-                SEmail: $("#SEmail").val()
-            };
-            $.post("../Controller/Verify_SignUpStudent.php", data, function(result) {
-                $("#studentMessage").html(result.Message);
-                setTimeout(() => {
-                    $("#studentMessage").html(" ");
-                }, 4000);
-            });
-
-        }
 
         function createTeacher() {
             var data = {
