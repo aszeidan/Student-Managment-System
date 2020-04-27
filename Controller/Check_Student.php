4edaebfec43e88ecde1170689d5c8a7d950911af
Ghida Shaaban <ghidashaaban@gmail.com>
2020-04-27 17:53:08 +0300

    Create Check_Student.php

---
+++ Controller/Check_Student.php
@@ -0,0 +1,28 @@
+
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+try{
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+	require_once('../Model/DatabaseSMS.php');
+	require_once('../Model/Admin.php');
+	$db = new DatabaseSMS();
+	$Admin = new Admin($db);
+	$del_id = $_GET['StudentId'];
+    $Admin->getDeletedId($del_id);
+
+	$depencies = $Admin->isThereStudentDependencies();
+	$output["error"]="0";
+		if($depencies){			
+			$output["result"]="0";
+		}else{
+			$Admin->deleteStudentById();
+			$output["result"]="1";			
+		}
+}catch(Exception $e)
+{
+	 $output["error"]='Caught exception: '. $e->getMessage();
+}
+ echo json_encode($output);
