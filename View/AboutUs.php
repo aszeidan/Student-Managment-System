280c93f5db1168a83a9c2145dd60654fecbea3e3
nadinekd <61241713+nadinekd@users.noreply.github.com>
2020-05-07 13:49:17 +0300

    updat about us

--- View/AboutUs.php
+++ View/AboutUs.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <?php
-require_once('Header.php');
+require_once('HeaderSignin.php');
 ?>
 
 <body>
@@ -19,7 +19,7 @@ require_once('Header.php');
       <!-- Team item-->
       <div class="col-xl-3 col-sm-6 mb-5">
         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/AmiraZaidan.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Amira Zaidan </h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <h5 class="mb-0">Amira Zaidan </h5><span class="small text-uppercase text-muted">Bachelor of Science - BS Computer Science</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f white-text mr-4"></i></a></li>
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
@@ -33,7 +33,7 @@ require_once('Header.php');
       <!-- Team item-->
       <div class="col-xl-3 col-sm-6 mb-5">
         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/GhidaShaaban.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Ghida Shaaban</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <h5 class="mb-0">Ghida Shaaban</h5><span class="small text-uppercase text-muted"> Master of Science Degree,     Statistics</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f white-text mr-4" aria-hidden="true" ></i></a></li>
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
@@ -47,7 +47,7 @@ require_once('Header.php');
       <!-- Team item-->
       <div class="col-xl-3 col-sm-6 mb-5">
         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/IhabBreich.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Ihab Breich</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <h5 class="mb-0">Ihab Breich</h5><span class="small text-uppercase text-muted">Bachelor's Degree,Computer Science</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="https://www.facebook.com/ihab.breich.5/timeline?lst=1666338536%3A100007344398323%3A1588845665" class="social-link"><i class="fa fa-facebook-f white-text mr-4"></i></a></li>
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
@@ -61,7 +61,7 @@ require_once('Header.php');
       <!-- Team item-->
       <div class="col-xl-3 col-sm-6 mb-5">
         <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/NadineKamalEddine.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-          <h5 class="mb-0">Nadine Kamal Eddine</h5><span class="small text-uppercase text-muted">CEO - Founder</span>
+          <h5 class="mb-0">Nadine Kamal Eddine</h5><span class="small text-uppercase text-muted">Master Degree, Software Developer</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f white-text mr-4"></i></a></li>
             <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
