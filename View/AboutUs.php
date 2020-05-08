6367fa9c50a8356543b96c6506fc3586c100a418
nadinekd <nadinekamaleddine@hotmail.com>
2020-05-08 19:03:03 +0300

    update about us

--- View/AboutUs.php
+++ View/AboutUs.php
@@ -8,23 +8,38 @@ require_once('HeaderSignin.php');
 <body>
   <div class="bg-light py-5">
     <div class="container py-5">
-      <div class="row mb-4">
-        <div class="col-lg-5">
-          <h2 class="display-4 font-weight-light">Our team</h2>
-          <p class="font-italic text-muted">We are all very different. We were born in different places, at different times, we love different music, food, movies. But we have something that unites us all. It is CODING. Its our heart. We are not just a team, we are a family.</p>
-        </div>
-      </div>
-
+		<div class="row mb-4">
+			<div class="col-lg-5">
+			  <h2 class="display-4 font-weight-light">Our team</h2>
+			  <p class="font-italic text-muted">We are all very different. We were born in different places, at different times, we love different music, food, movies. But we have something that unites us all. It is CODING. Its our heart. We are not just a team, we are a family.</p>
+			</div>
+			<div class="col-lg-1" ></div>
+			<div class="col-lg-6">
+				<h2 class="display-4 font-weight-light">Our Contributors</h2>
+				<span>
+					<a href="https://park-innovation.com/?ref=lebanesetech" class="navbar-brand">
+					  <!-- Logo Image -->
+					  <img src="../images/Park-Innovation-avatar.jpg" width="100" alt="" class="d-inline-block align-middle mr-2">
+					</a>
+				</span>
+				<span>
+					<a href="https://www.techgenies.com/" class="navbar-brand">
+					  <!-- Logo Image -->
+					  <img src="../images/Tech_genies.jpg" width="200" alt="" class="d-inline-block align-middle mr-2">
+					</a>
+				</span>
+			</div>
+		</div>
       <div class="row text-center">
         <!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
           <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/AmiraZaidan.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Amira Zaidan </h5><span class="small text-uppercase text-muted">BS Degree in Computer Science</span>
+            <h5 class="mb-0">Amira Zaidan </h5><span class="small text-uppercase text-muted">BS Degree <br> Computer Science</span>
             <ul class="social mb-0 list-inline mt-3">
               <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f "></i></a></li>
               <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="https://https://www.instagram.com/belle_ame_fashion1/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="https://linkedin.com/in/amira-s-zeidan-9ba0b0160" class="social-link"><i class="fa fa-linkedin"></i></a></li>
             </ul>
           </div>
         </div>
@@ -33,12 +48,12 @@ require_once('HeaderSignin.php');
         <!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
           <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/GhidaShaaban.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Ghida Shaaban</h5><span class="small text-uppercase text-muted"> MS Degree in Statistics</span>
+            <h5 class="mb-0">Ghida Shaaban</h5><span class="small text-uppercase text-muted"> MS Degree <br> Statistics</span>
             <ul class="social mb-0 list-inline mt-3">
               <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook-f " aria-hidden="true"></i></a></li>
               <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="https://https://www.instagram.com/ghida.shaaban/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="linkedin.com/in/ghidashaabann" class="social-link"><i class="fa fa-linkedin"></i></a></li>
             </ul>
           </div>
         </div>
@@ -47,12 +62,12 @@ require_once('HeaderSignin.php');
         <!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
           <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/IhabBreich.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Ihab Breich</h5><span class="small text-uppercase text-muted">BS Degree in Computer Science</span>
+            <h5 class="mb-0">Ihab Breich</h5><span class="small text-uppercase text-muted">BS Degree <br>Computer Science</span>
             <ul class="social mb-0 list-inline mt-3">
               <li class="list-inline-item"><a href="https://www.facebook.com/ihab.breich.5/timeline?lst=1666338536%3A100007344398323%3A1588845665" class="social-link"><i class="fa fa-facebook-f "></i></a></li>
               <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin"></i></a></li>
+              <li class="list-inline-item"><a href="https://https://www.instagram.com/ihab_breich/" class="social-link"><i class="fa fa-instagram"></i></a></li>
+              <li class="list-inline-item"><a href="https://linkedin.com/in/ihab-breich-639507162" class="social-link"><i class="fa fa-linkedin"></i></a></li>
             </ul>
           </div>
         </div>
@@ -61,12 +76,12 @@ require_once('HeaderSignin.php');
         <!-- Team item-->
         <div class="col-xl-3 col-sm-6 mb-5">
           <div class="bg-white rounded shadow-sm py-5 px-4"><img src="../images/NadineKamalEddine.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
-            <h5 class="mb-0">Nadine Kamal Eddine</h5><span class="small text-uppercase text-muted">MS Degree in Software Developer</span>
+            <h5 class="mb-0">Nadine Kamal Eddine</h5><span class="small text-uppercase text-muted">MS Degree <br> Software Developer</span>
             <ul class="social mb-0 list-inline mt-3">
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-facebook "></i></a></li>
+              <li class="list-inline-item"><a href="https://www.facebook.com/nadoun12" class="social-link"><i class="fa fa-facebook "></i></a></li>
               <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-twitter"></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-instagram "></i></a></li>
-              <li class="list-inline-item"><a href="#" class="social-link"><i class="fa fa-linkedin "></i></a></li>
+              <li class="list-inline-item"><a href="https://www.instagram.com/nadokd/" class="social-link"><i class="fa fa-instagram "></i></a></li>
+              <li class="list-inline-item"><a href="https://linkedin.com/in/nadine-kamal-eddine-9a2b17158" class="social-link"><i class="fa fa-linkedin "></i></a></li>
             </ul>
           </div>
         </div>
