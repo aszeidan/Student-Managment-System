7a0716049e1868a0eafd17b8169b692198bba843
Ghida Shaaban <ghidashaaban@gmail.com>
2020-05-13 00:54:44 +0300

    Update Student_Registration.php

--- View/Student_Registration.php
+++ View/Student_Registration.php
@@ -1,5 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
 <?php
+
 require_once('Header.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 require_once('../Model/Student.php');
 require_once('../Model/DatabaseSMS.php');
@@ -14,9 +20,6 @@ $Student->setStudentId($studentID);
 $semester = $Admin->getSemesters();
 ?>
 
-<html lang="en">
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
 <script>
     function getCoursesBySemester(SemesterId) {
         var semesterID = $("#SemesterId").val();
@@ -28,10 +31,33 @@ $semester = $Admin->getSemesters();
 </script>
 
 <body>
-    <div class="registration">
-        <h2 class="py-3">Drop/Add Courses</h2>
-
-        <div>
+<div class="register">
+        <div class="row">
+            <div class="col-md-3 register-left">
+                <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
+                <h3>Welcome To</h3>
+                <h3><b>Time Travel University</b></h3>
+                <P>We look forward to welcoming you to our campus soon!​</P>
+                <form action="../Controller/Logout.php" method="POST">
+                    <input class="signout" type="submit" name="" value="SignOut" style="background:white; color:#0062cc; cursor:pointer" /><br />
+                    <style>
+                        .signout:hover {
+                            background: #0062cc;
+                            color: white;
+                            box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+                            transform: translateY(-7px);
+                        }
+                    </style>
+                </form>
+            </div>
+			 <div class="col-md-9 register-rights">
+                <div class="tab-content" id="myTabContent">
+                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                        <h3 class="register-heading"> Drop/Add Courses</h3>
+                        <div class="row register-forms mx-0 px-0 col-md-12">
+                            <div class="centering col-md-6">
+                                
+            <form>
             <select class="mb-3" id="SemesterId" onChange="getCoursesBySemester()" name="Semester" required>
                 <option disabled value="" selected hidden>Select Semester</option>
                 <?php
@@ -41,8 +67,9 @@ $semester = $Admin->getSemesters();
                 <?php } ?>
             </select>
         </div>
-        <div id="classSemester">
+        <div id="classSemester" >
         </div>
+        </form>
     </div>
 </body>
 
